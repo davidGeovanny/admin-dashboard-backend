0432530c6a24cf420c4a3e933700e8fa24205e4c
David Geovanny <gcarrillo@cafica.com.mx>
2021-09-30 17:12:42 -0600

    Model sales (in progress)

---
+++ models/sale.js
@@ -0,0 +1,6 @@
+const db = require('../db/connection');
+const { DataType } = require('sequelize');
+
+const Sale = db.define('Sale', {
+  
+});
--- models/server.js
+++ models/server.js
@@ -7,7 +7,7 @@ require('./index');
 
 class Server {
   constructor() {
-    this.app = express();
+    this.app  = express();
     this.port = process.env.PORT || 8080;
 
     this.paths = {
